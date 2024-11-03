be9f7e153a59ae13e7d513238f91ffd8d3eec102
Kristaps Zarins <kzarins@2-C-1.42heilbronn.de>
2024-11-03 18:31:41 +0100

    Added bonus for s handling

--- format_handle/s_handle.c
+++ format_handle/s_handle.c
@@ -6,7 +6,7 @@
 /*   By: kzarins <kzarins@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:15:26 by kzarins           #+#    #+#             */
-/*   Updated: 2024/11/03 04:16:43 by kzarins          ###   ########.fr       */
+/*   Updated: 2024/11/03 18:27:17 by kzarins          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,24 @@
 
 int	s_handle(char *str, int field_width, int format_flags)
 {
+	int	width_count;
+
+	width_count = field_width;
 	if (!str)
 		return (-1);
+	if ((format_flags & (WITH_PADDING | RIGHT_PAD)) == WITH_PADDING)
+		while (width_count > ft_strlen(str))
+		{
+			write(STD_OUT, " ", 1);
+			width_count--;
+		}
 	ft_putstr_fd(str, STD_OUT);
+	while (width_count > ft_strlen(str))
+	{
+		write(STD_OUT, " ", 1);
+		width_count--;
+	}
+	if (field_width > ft_strlen(str))
+		return(field_width);
 	return (ft_strlen(str));
 }
