c79d025d9f0683c3f58298212867623dd798279e
Kristaps Zarins <kzarins@2-D-1.42heilbronn.de>
2024-11-04 04:30:57 +0100

    Now precision of 0 is handled

--- format_handle/i_handle.c
+++ format_handle/i_handle.c
@@ -6,12 +6,13 @@
 /*   By: kzarins <kzarins@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 19:35:11 by kzarins           #+#    #+#             */
-/*   Updated: 2024/11/03 22:41:26 by kzarins          ###   ########.fr       */
+/*   Updated: 2024/11/04 04:30:08 by kzarins          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "format_handle.h"
 
 static char	*add_num_precision(char *int_in_char, int precision);
+static char	*add_signe(char *int_in_char, int num, int format_flags);
 static void	add_front_spacing(int *width_left, int len, int format_flags);
 static void	add_back_spacing(int width_left, int len, int format_flags);
 
@@ -21,11 +22,19 @@ int	i_handle(int num, struct s_field_specifiers field, int format_flags)
 	int		len;
 	int		width_left;
 
-	int_in_char = ft_itoa(num);
+	if (num == 0 && field.precision == 0)
+	{
+		int_in_char = ft_strdup("");
+		if (!int_in_char)
+			return (-1);
+	}
+	else
+		int_in_char = ft_itoa(num);
 	width_left = field.field_width;
 	if (!int_in_char)
 		return (-1);
 	int_in_char = add_num_precision(int_in_char, field.precision);
+	int_in_char = add_signe(int_in_char, num, format_flags);
 	len = ft_strlen(int_in_char);
 	add_front_spacing(&width_left, len, format_flags);
 	write(STD_OUT, int_in_char, len);
@@ -60,6 +69,30 @@ static char	*add_num_precision(char *int_in_char, int precision)
 			return (int_in_char);
 		}
 		free(int_in_char);
+	}
+	return (result);
+}
+
+static char	*add_signe(char *int_in_char, int num, int format_flags)
+{
+	char	*result;
+
+	result = int_in_char;
+	if (num >= 0 && (format_flags & PLUS_SING))
+	{
+		result = ft_strjoin("+", int_in_char);
+		if (!result)
+			return (int_in_char);
+		free (int_in_char);
+		return (result);
+	}
+	else if (num >= 0 && (format_flags & SPACE))
+	{
+		result = ft_strjoin(" ", int_in_char);
+		if (!result)
+			return (int_in_char);
+		free (int_in_char);
+		return (result);
 	}
 	return (result);
 }
